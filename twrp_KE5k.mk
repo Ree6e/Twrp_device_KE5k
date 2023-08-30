a758cf5a9f5a7dd4bd140f9c31b4f19423f656d9
Reee22 <93215371+Ree6e@users.noreply.github.com>
2023-08-31 00:47:28 +0800

    Update and rename omni_TECNO-KE5k.mk to twrp_KE5k.mk

--- twrp_KE5k.mk
+++ twrp_KE5k.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from TECNO-KE5k device
-$(call inherit-product, device/tecno/TECNO-KE5k/device.mk)
+# Inherit from KE5k device
+$(call inherit-product, device/tecno/KE5k/device.mk)
 
-PRODUCT_DEVICE := TECNO-KE5k
-PRODUCT_NAME := omni_TECNO-KE5k
+PRODUCT_DEVICE := KE5k
+PRODUCT_NAME := twrp_KE5k
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KE5k
 PRODUCT_MANUFACTURER := tecno
